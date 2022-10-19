2ed59565874259c4c4eeace23f69269b12fb4e65
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-10-19 14:00:04 +0900

    Feat NodeContainer에서 NodeCanvas로 이름 변경

--- components/Mindmap/index.jsx
+++ components/Mindmap/index.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 
 import Header from '../Header';
-import SearchBar from '../SearchBar';
 
-const NodeContainer = dynamic(() => import('../NodeContainer'), {
+const NodeCanvas = dynamic(() => import('../NodeCanvas'), {
   ssr: false,
 });
 
@@ -12,8 +11,7 @@ export default function MindMap() {
   return (
     <>
       <Header />
-      <SearchBar />
-      <NodeContainer />
+      <NodeCanvas />
     </>
   );
 }
