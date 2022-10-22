282e2335ef38c6654c554a2da0d68e2a6ad3a265
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-10-22 22:34:57 +0900

    Feat login 컴포넌트 작성 로그인 후 store state에 유저 데이터 추가

--- components/Login/index.jsx
+++ components/Login/index.jsx
@@ -1,25 +1,85 @@
 import React from 'react';
+import Image from 'next/image';
 
 import styled from 'styled-components';
+import { useSetRecoilState } from 'recoil';
+import { useRouter } from 'next/router';
+import { currentUserInfo } from '../../store/states';
 
+import { login } from '../../service/auth';
+import googleDocsIcon from '../../public/images/btn_google_signin_light_pressed_web@2x.png';
 import Header from '../Header';
-import Button from '../shared/Button';
+import flexCenter from '../shared/FlexCenterContainer';
 
 export default function Login() {
+  const saveUserData = useSetRecoilState(currentUserInfo);
+  const router = useRouter();
+
+  const clickLoginHandler = async () => {
+    const response = await login();
+    if (!response?.user) {
+      return;
+    }
+    const { _id, username, email, profile } = response.user;
+    saveUserData({
+      id: _id,
+      username,
+      email,
+      profile,
+    });
+    if (response.result === 'ok') {
+      router.push('/');
+    }
+  };
+
   return (
     <Wrapper>
       <Header />
-      <Button>구글로그인</Button>
+      <IconWrapper>
+        <Title>Sign in with</Title>
+        <Title>Google</Title>
+
+        <Image
+          src={googleDocsIcon}
+          alt="google-docs-icon"
+          className="googleLoginButton"
+          onClick={clickLoginHandler}
+          placeholder="empty"
+        />
+      </IconWrapper>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100%;
-  width: 100%;
+  position: relative;
+  margin: 0;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  text-align: left;
+  margin: 0;
+`;
+
+const IconWrapper = styled(flexCenter)`
+  min-width: 10rem;
+  height: 80vh;
+  align-items: flex-start;
+  transform: translateY(-30px);
+
+  .googleLoginButton {
+    width: 100%;
+    margin-top: 3rem;
+    transition: 0.1s all;
+    cursor: pointer;
+
+    &:hover {
+      transform: scale(1.02);
+    }
+  }
 `;
