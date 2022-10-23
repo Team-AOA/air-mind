a2d4072da4efd3cf41d6589c5770523e914a5abb
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-10-24 06:17:18 +0900

    Refactor 에러페이지 수정 next 설정상 page에 바로 404.js, 500.js 파일이 있으면 404, 500에러가 났을 때 바로 그 파일을 렌더링한다고 합니다. 현재 모든 에러페이지가 우리가 커스텀한 에러페이지로만 뜨지 않고 next js의 기본 에러페이지와 섞여서 렌더링 되고 있어 수정합니다.

---
+++ pages/404.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import NotFoundPage from '../components/Error/404';
+
+export default function LoginPage() {
+  return <NotFoundPage />;
+}
---
+++ pages/500.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import InternalError from '../components/Error/500';
+
+export default function LoginPage() {
+  return <InternalError />;
+}
