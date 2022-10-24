2fc09073b9fa5b42ff438eb4ea81a581ab36be43
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-10-24 23:59:17 +0900

    Refactor profile 이미지 에러떠서 수정

--- components/shared/ProfileIcon.jsx
+++ components/shared/ProfileIcon.jsx
@@ -1,9 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const ProfileIcon = styled.img`
+const Icon = styled.img`
   width: 50px;
   height: 50px;
   border-radius: 50px;
 `;
 
-export default ProfileIcon;
+const GuestIcon = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  background-color: gray;
+`;
+
+export default function ProfileIcon({ src, alt }) {
+  const [hasUrl, setHasUrl] = useState(false);
+
+  useEffect(() => {
+    if (src) {
+      setHasUrl(true);
+    }
+  }, []);
+
+  return hasUrl ? <Icon src={src} alt={alt} /> : <GuestIcon />;
+}
+
+ProfileIcon.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
