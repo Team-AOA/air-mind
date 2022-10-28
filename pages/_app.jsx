978740ab7f1e729ee797c1aaeaa925cc1bd6362d
Sharpen-Cjh <101804186+Sharpen-Cjh@users.noreply.github.com>
2022-10-28 22:04:19 +0900

    Feat 마인드맵페이지 메타태그 추가

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
 
+import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 import PropTypes from 'prop-types';
 import GlobalStyle from '../components/shared/globalstyle';
+// import getPublicMindMapData from '../service/mindmaprequests';
 
 function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
+      <Head>
+        <title>에어마인드</title>
+        <meta
+          property="keywords"
+          content="마인드맵, 협업툴, 회의, 마인드맵 회의, 브레인스토밍, 실시간 회의, 실시간 마인드맵"
+        />
+        <meta
+          property="description"
+          content="실시간으로 마인드맵을 이용하여 회의를 진행하거나 아이디어를 공유할 수 있습니다."
+        />
+        {/* {mindMaps?.map(mindMap => (
+          <>
+            <meta name="description" content={mindMap?.content} />
+            <title>{mindMap?.title}</title>
+          </>
+        ))} */}
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </RecoilRoot>
   );
 }
+// export async function getStaticProps() {
+//   const data = await getPublicMindMapData();
+
+//   const mindMaps = data.mindMap;
+
+//   return {
+//     props: {
+//       mindMaps,
+//     },
+//   };
+// }
 
 export default MyApp;
 
 MyApp.propTypes = {
   Component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
   pageProps: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
+  // mindMaps: PropTypes.array.isRequired,
 };
--- pages/mind-map/[mindMapId].jsx
+++ pages/mind-map/[mindMapId].jsx
@@ -1,11 +1,57 @@
 import React from 'react';
-import { useRouter } from 'next/router';
-
+import PropTypes from 'prop-types';
+import Head from 'next/head';
 import MindMap from '../../components/mindmap';
+import getPublicMindMapData from '../../service/mindmaprequests';
 
-export default function MindMapPage() {
-  const router = useRouter();
-  const { mindMapId } = router.query;
+export default function MindMapPage({ mindMaps, mindMapId }) {
+  const metaMindMap = mindMaps.filter(mindMap => {
+    const { _id: id } = mindMap;
+    return id === mindMapId;
+  });
 
-  return <MindMap mindMapId={mindMapId || ''} />;
+  return (
+    <>
+      <Head />
+      <meta name="title" title={metaMindMap[0].title} />
+      <meta name="description" content={metaMindMap[0].access} />
+      <MindMap mindMapId={mindMapId} />;
+    </>
+  );
 }
+
+export async function getStaticProps(context) {
+  const { mindMapId } = context.params;
+  const data = await getPublicMindMapData();
+
+  const mindMaps = await data.mindMap;
+
+  return {
+    props: {
+      mindMaps,
+      mindMapId,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getPublicMindMapData();
+  const mindMaps = await data.mindMap;
+  const paths = [];
+
+  mindMaps.map(mindMap => {
+    const { _id: mindMapId } = mindMap;
+    paths.push({ params: { mindMapId } });
+    return mindMap;
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+MindMapPage.propTypes = {
+  mindMaps: PropTypes.array.isRequired,
+  mindMapId: PropTypes.string.isRequired,
+};
